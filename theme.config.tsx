4d954d492e35fd8017d727a21e847f4d3765989c
Not TCA <realtcatech@gmail.com>
2023-11-30 20:18:56 +0800

    styling

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from "next/image";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import React from "react";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <Image src="/logo.png" alt="csmos logo" width={40} height={40} />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – csmos",
+    };
+  },
+  primaryHue: 291,
+};
 
-export default config
+export default config;
