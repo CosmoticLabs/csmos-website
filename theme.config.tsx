b4c0c2887ff2311b227ce0aeee38b04754be76bf
Not TCA <realtcatech@gmail.com>
2023-11-30 20:28:03 +0800

    update theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,12 +5,19 @@ import React from "react";
 const config: DocsThemeConfig = {
   logo: <Image src="/logo.png" alt="csmos logo" width={40} height={40} />,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/CosmoticLabs/csmos",
   },
-  chat: {
-    link: "https://discord.com",
+  // chat: {
+  //   link: "https://discord.com",
+  // },
+  docsRepositoryBase: "https://github.com/CosmoticLabs/csmos-website/edit/main",
+  editLink: {
+    text: "Edit this page on GitHub →",
   },
-  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+  feedback: {
+    content: "Question? Give us feedback →",
+    labels: "feedback",
+  },
   footer: {
     text: "Nextra Docs Template",
   },
