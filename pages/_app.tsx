6f4e919ada991db09859e65975fb6871948e6e30
Not TCA <realtcatech@gmail.com>
2023-12-01 17:33:26 +0800

    move to inter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 
-import { IBM_Plex_Sans } from "next/font/google";
-const font = IBM_Plex_Sans({
+import { Inter } from "next/font/google";
+const font = Inter({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
